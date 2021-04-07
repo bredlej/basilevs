d5da00ecf70d57c5246d4244765119964e535127
Patryk Szczypien <patryk.szczypien@gmail.com>
2021-04-07 13:16:03 +0200

    - initial window setup with raylib and raylib-cpp submodules - rendering to 160x144px texture which is then rendered scaled on the screen dimensions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
-#include <iostream>
-
+#include <raylib.hpp>
+#include <raylib-cpp.hpp>
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "raylib.h"
+#ifdef __cplusplus
+}
+#endif
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int screenWidth = 1900;
+    int screenHeight = 1600;
+
+    raylib::Window window(screenWidth, screenHeight, "raylib-cpp - basic window");
+
+    auto target = raylib::RenderTexture2D(160, 144);
+    //SetTextureFilter(target.texture, FILTER_BILINEAR);
+    SetTargetFPS(60);
+
+    while (!window.ShouldClose())
+    {
+        BeginDrawing();
+        BeginTextureMode(target);
+        window.ClearBackground(BLACK);
+        DrawText("BASILEVS", 20, 20, 20, LIGHTGRAY);
+        EndTextureMode();
+        DrawTexturePro(target.texture, (Rectangle){ 0.0f, 0.0f, (float)target.texture.width, (float)-target.texture.height },
+                       (Rectangle){ 0.0f, 0.0f, static_cast<float>(screenWidth), static_cast<float>(screenHeight) }, (Vector2){ 0, 0 }, 0.0f, WHITE);
+        EndDrawing();
+    }
+
     return 0;
 }
