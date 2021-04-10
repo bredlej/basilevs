9441b5f15e5badb54ae24229cc6834263007d642
Patryk Szczypien <patryk.szczypien@gmail.com>
2021-04-11 01:15:43 +0200

    - implemented enemy spawn functionality

--- include/core.h
+++ include/core.h
@@ -4,19 +4,29 @@
 
 #ifndef BASILEVS_CORE_H
 #define BASILEVS_CORE_H
+#include <functional>
 #include <raylib-cpp.hpp>
+#include <utility>
 namespace core {
-    struct PlayerDefinition {
+    struct Sprite {
     public:
-        PlayerDefinition(const std::string &animation_file, Vector2 &&position, uint8_t amount_frames)
+        Sprite(const std::string &animation_file, Vector2 &&position, uint8_t amount_frames)
             : texture{LoadTextureFromImage(raylib::LoadImage(animation_file))},
               position{position},
               current_frame_{0},
               frame_rect{0.0f, 0.0f, static_cast<float>(texture.width) / amount_frames, static_cast<float>(texture.height)},
               amount_frames_{amount_frames} {};
+        Sprite(const Texture2D &texture, Vector2 &&position, uint8_t amount_frames)
+            : texture{texture},
+              position{position},
+              current_frame_{0},
+              frame_rect{0.0f, 0.0f, static_cast<float>(texture.width) / amount_frames, static_cast<float>(texture.height)},
+              amount_frames_{amount_frames} {};
+
         Vector2 position;
         Texture2D texture;
         Rectangle frame_rect;
+        float inner_timer{0.0f};
 
         void update_animation(int frame_speed) {
             frame_counter_++;
@@ -39,28 +49,37 @@ namespace core {
 
     struct Background {
     public:
-        Background(const std::string &file_name, int vertical_segments)
+        Background(const std::string &file_name, const int vertical_segments)
             : texture{LoadTextureFromImage(raylib::LoadImage(file_name))},
-              vertical_segments_{vertical_segments},
+              amount_frames{vertical_segments},
               texture_rect{0.0, static_cast<float>(texture.height - (texture.height / vertical_segments)), static_cast<float>(texture.width), static_cast<float>((texture.height / vertical_segments))},
               frame_counter_{0} {};
         Texture2D texture;
         Rectangle texture_rect;
 
-        void update_background(float frame_speed, float frame_time) {
-            frame_counter_++;
+        void update_background(const float frame_speed, const float frame_time) {
+            frame_counter_ += 1;
             if (texture_rect.y > 0) {
                 if (frame_counter_ >= (60 / frame_speed)) {
                     frame_counter_ = 0;
-
-                    texture_rect.y -=1 - frame_time;
+                    texture_rect.y -= 1 - frame_time;
                 }
             }
         }
+
     private:
-        int vertical_segments_;
-        int offset;
+        int amount_frames;
         int frame_counter_;
+    };
+
+    struct Enemy {
+        using BehaviorFunction = std::function<void(Enemy &, double)>;
+
+    public:
+        Enemy(Sprite animation, BehaviorFunction behavior, Vector2 position) : animation{animation}, behavior{std::move(behavior)}, position{position} {};
+        Sprite animation;
+        BehaviorFunction behavior;
+        Vector2 position;
     };
 }// namespace core
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <raylib.hpp>
 #include <raylib-cpp.hpp>
+#include <raylib.hpp>
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -9,24 +9,39 @@ extern "C" {
 #endif
 
 #include "include/core.h"
-
+#include <algorithm>
+#include <chrono>
+#include <functional>
+#include <iostream>
+#include <list>
 int main() {
+
+    double timer = 0;
 
     int frameWidth = 160;
     int frameHeight = 144;
     int screenWidth = 800;
     int screenHeight = 760;
 
+    raylib::Window window(screenWidth, screenHeight, "Basilevs");
+
     auto fgColor = raylib::Color(240, 246, 240);
     auto bgColor = raylib::Color(34, 35, 35);
 
-    raylib::Window window(screenWidth, screenHeight, "Basilevs");
+    //auto basilevs_logo = LoadTextureFromImage(raylib::LoadImage("assets/basilevs.png"));
 
-    auto basilevs_logo = LoadTextureFromImage(raylib::LoadImage("assets/basilevs.png"));
+    auto render_target = raylib::RenderTexture2D(160, 144);
 
-    auto target = raylib::RenderTexture2D(160, 144);
-    auto player = core::PlayerDefinition("assets/player.png", raylib::Vector2{64, 90}, 7);
+    auto player_texture = LoadTextureFromImage(raylib::LoadImage("assets/player.png"));
+    auto player_sprite = core::Sprite(player_texture, raylib::Vector2{64, 90}, 7);
+
+    auto enemy_texture = LoadTextureFromImage(raylib::LoadImage("assets/enemy.png"));
+    auto enemy_sprite = core::Sprite(enemy_texture, raylib::Vector2(70, 30), 1);
+
+    auto bullet8_texture = LoadTextureFromImage(raylib::LoadImage("assets/bullet8.png"));
+    auto bullet_sprite = core::Sprite(bullet8_texture, raylib::Vector2(50, 50), 1);
     auto background = core::Background("assets/basilevs_bg_001.png", 6);
+
     SetTextureFilter(background.texture, FILTER_ANISOTROPIC_16X);
     SetTargetFPS(60);
 
@@ -34,35 +49,68 @@ int main() {
 
     int movement_speed = 60;
 
-    while (!window.ShouldClose())
-    {
+    auto clock = std::chrono::steady_clock{};
+
+    struct Spawn {
+        float start_time;
+        Vector2 position;
+        core::Sprite enemy;
+        std::function<void(core::Enemy &, float)> behavior;
+    };
+
+    auto behavior_sinusoidal = [&](core::Enemy &enemy, double time) -> void {
+        enemy.position.x += sin(time) * 0.7;
+    };
+
+    auto enemy_spawns = std::list<Spawn>({{5.0f, Vector2{60.0, 10.0}, enemy_sprite, behavior_sinusoidal},
+                                          {8.0f, Vector2{60.0, 60.0}, bullet_sprite, behavior_sinusoidal}});
+
+    auto enemies_on_screen = std::list<core::Enemy>{};
+
+    while (!window.ShouldClose()) {
+        auto now = std::chrono::steady_clock::now();
+        if (!enemy_spawns.empty()) {
+            auto next_spawn = enemy_spawns.front();
+            if (timer >= next_spawn.start_time) {
+                enemies_on_screen.emplace_back(core::Enemy{next_spawn.enemy, behavior_sinusoidal, next_spawn.position});
+                enemy_spawns.pop_front();
+            }
+        }
         BeginDrawing();
-        BeginTextureMode(target);
+        BeginTextureMode(render_target);
         window.ClearBackground(bgColor);
-        //DrawTexture(player.texture, static_cast<int>(player.position.x), static_cast<int>(player.position.y), WHITE);
         DrawTextureRec(background.texture, background.texture_rect, Vector2{0.0f, 0.0f}, WHITE);
-        DrawTextureRec(player.texture, player.frame_rect, player.position, WHITE);
-        player.update_animation(12);
+        DrawTextureRec(player_sprite.texture, player_sprite.frame_rect, player_sprite.position, WHITE);
+
+        player_sprite.update_animation(12);
         background.update_background(12, GetFrameTime());
 
         if (move_right) {
-            player.position.x += 1 * GetFrameTime() * movement_speed;
-            if (player.position.x >= frameWidth - player.frame_rect.width) {
+            player_sprite.position.x += 1 * GetFrameTime() * movement_speed;
+            if (player_sprite.position.x >= frameWidth - player_sprite.frame_rect.width) {
                 move_right = false;
             }
-        }
-        else {
-            player.position.x -= 1 * GetFrameTime() * movement_speed;
-            if (player.position.x <= 0) {
+        } else {
+            player_sprite.position.x -= 1 * GetFrameTime() * movement_speed;
+            if (player_sprite.position.x <= 0) {
                 move_right = true;
             }
         }
 
+        for (auto &enemy : enemies_on_screen) {
+            DrawTextureRec(enemy.animation.texture, enemy.animation.frame_rect, enemy.position, WHITE);
+            enemy.behavior(enemy, timer);
+        }
         EndTextureMode();
-        DrawTexturePro(target.texture, (Rectangle){ 0.0f, 0.0f, (float)target.texture.width, (float)-target.texture.height },
-                       (Rectangle){ 0.0f, 0.0f, static_cast<float>(screenWidth), static_cast<float>(screenHeight) }, (Vector2){ 0, 0 }, 0.0f, WHITE);
+        DrawTexturePro(render_target.texture, (Rectangle){0.0f, 0.0f, (float) render_target.texture.width, (float) -render_target.texture.height},
+                       (Rectangle){0.0f, 0.0f, static_cast<float>(screenWidth), static_cast<float>(screenHeight)}, (Vector2){0, 0}, 0.0f, WHITE);
+        raylib::DrawText(std::to_string(static_cast<int>(timer)), screenWidth - 60, 20, 30, GREEN);
         EndDrawing();
+        std::chrono::duration<double> elapsed = std::chrono::steady_clock::now() - now;
+
+        timer += elapsed.count();
     }
 
+    std::cout << "Done after: " << timer << " seconds " << std::endl;
     return 0;
 }
