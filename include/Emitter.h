c0c1fda2b223a219248130b0410949153f136b7f
Patryk Szczypien <patryk.szczypien@gmail.com>
2021-04-27 19:48:44 +0200

    - code refactor in Emitter.h

--- include/Emitter.h
+++ include/Emitter.h
@@ -9,6 +9,7 @@
 #include <concepts>
 #include <functional>
 #include <iostream>
+#include <type_traits>
 
 struct World;
 
@@ -36,20 +37,6 @@ struct EmitterBase {
 template<typename T>
 concept is_an_emitter = std::is_base_of_v<EmitterBase, T>;
 
-template<is_many_components... Ts>
-class TEmitter : EmitterBase {
-    using EmitterFunction = std::function<void(const double time, TEmitter<Ts...> &emitter, World &world)>;
-    using ComponentFunction = std::function<void(const double, World &, Ts...)>;
-    using Components = std::tuple<Ts...>;
-public:
-    explicit TEmitter(int id) : id{id} {};
-    explicit TEmitter(int id, ComponentFunction func) : id{id}, component_function{func} {};
-    int id{0};
-    EmitterFunction function;
-    ComponentFunction component_function;
-    Components components = std::make_tuple(Ts()...);
-};
-
 template<is_a_component T>
 auto &get(is_an_emitter auto &emitter) {
     return std::get<T>(emitter.components);
@@ -65,23 +52,41 @@ auto &get(is_a_memory auto &memory) {
 }
 
 template<is_many_components... Ts>
+class TEmitter : EmitterBase {
+    using ComponentFunction = std::function<void(const double, World &, Ts&...)>;
+    using Components = std::tuple<Ts...>;
+public:
+    explicit TEmitter(int id, ComponentFunction func)
+        : id{id}, component_function{func} {};
+
+    int id{0};
+    ComponentFunction component_function;
+    Components components = std::make_tuple(Ts()...);
+};
+
+template<is_many_components... Ts>
 class EmitterMemory : EmitterMemoryBase {
     using Components = std::tuple<std::vector<Ts>...>;
-    using ComponentFunction = std::function<void(const double, World &, Ts...)>;
+    using ComponentFunction = std::function<void(const double, World &, Ts&...)>;
     using ComponentFunctionVector = std::vector<ComponentFunction>;
 public:
-    Components components;
-    ComponentFunctionVector functions;
     template <is_a_component T, is_an_emitter ... Emitters>
     static constexpr std::vector<T> unpack_components(Emitters const & ...emitter_pack) { return { std::get<T>(emitter_pack.components)... }; }
     static constexpr ComponentFunction unpack_function(is_an_emitter auto &emitter) { return emitter.component_function; }
     template<is_an_emitter ... Emitters>
     explicit EmitterMemory(Emitters const &...emitter_pack)
         : components { unpack_components<Ts>(emitter_pack...)... },
-          functions { unpack_function(emitter_pack)... } { };
+          functions{ unpack_function(emitter_pack)... } { };
+
+    Components components;
+    ComponentFunctionVector functions;
 };
 
 template <is_a_component ... Components, is_an_emitter ... Emitters>
 EmitterMemory (TEmitter<Components...>, Emitters...) -> EmitterMemory<Components...>;
 
+
+namespace functions {
+    static constexpr auto kEmptyFunction = [] (const double, World &, components::Position &, components::ActiveComponent &, components::ShootComponent &) -> void { };
+}
 #endif//BASILEVS_EMITTER_H
--- main.cpp
+++ main.cpp
@@ -15,17 +15,13 @@ int main() {
     auto window = raylib::Window{config::kScreenWidth, config::kScreenHeight, "Basilevs"};
     auto audio = raylib::AudioDevice();
 
-    auto emitter_func = [](const double, World &, components::Position, components::ActiveComponent, components::ShootComponent){};
-    auto emitter1 = TEmitter<components::Position, components::ActiveComponent, components::ShootComponent>{10};
-    auto &&position1 = get<components::Position>(emitter1);
+    auto emitter_func = std::function<void(const double, World &, components::Position &, components::ActiveComponent &, components::ShootComponent &)>(functions::kEmptyFunction);
+
+    auto emitter1 = TEmitter<components::Position, components::ActiveComponent, components::ShootComponent>(10, emitter_func);
+    auto &position1 = get<components::Position>(emitter1);
     position1.position = {10.0f, 10.0f};
-    auto &active1 = get<components::ActiveComponent>(emitter1);
-    active1.is_active = true;
-    auto emitter2 = TEmitter<components::Position, components::ActiveComponent, components::ShootComponent>(20, emitter_func);
-    auto &position2 = get<components::Position>(emitter2);
-    position2.position = {20.0f, 20.0f};
-    auto &active2 = get<components::ActiveComponent>(emitter2);
-    active2.is_active = false;
+
+    auto emitter2 = TEmitter(20, emitter_func);
     auto memory = EmitterMemory(emitter1, emitter2);
     auto &position_components = get<components::Position>(memory);
     position_components[0].position = {50.0f, 50.0f};
