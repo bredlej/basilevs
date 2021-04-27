fa9985b67ef4b691a9d6b0e517e0526c2c99da9d
Patryk Szczypien <patryk.szczypien@gmail.com>
2021-04-27 18:22:05 +0200

    - added unpacking of TEmitter Functions to vector in EmitterMemory

--- include/Emitter.h
+++ include/Emitter.h
@@ -39,11 +39,14 @@ concept is_an_emitter = std::is_base_of_v<EmitterBase, T>;
 template<is_many_components... Ts>
 class TEmitter : EmitterBase {
     using EmitterFunction = std::function<void(const double time, TEmitter<Ts...> &emitter, World &world)>;
+    using ComponentFunction = std::function<void(const double, World &, Ts...)>;
     using Components = std::tuple<Ts...>;
 public:
     explicit TEmitter(int id) : id{id} {};
+    explicit TEmitter(int id, ComponentFunction func) : id{id}, component_function{func} {};
     int id{0};
     EmitterFunction function;
+    ComponentFunction component_function;
     Components components = std::make_tuple(Ts()...);
 };
 
@@ -64,12 +67,18 @@ auto &get(is_a_memory auto &memory) {
 template<is_many_components... Ts>
 class EmitterMemory : EmitterMemoryBase {
     using Components = std::tuple<std::vector<Ts>...>;
+    using ComponentFunction = std::function<void(const double, World &, Ts...)>;
+    using ComponentFunctionVector = std::vector<ComponentFunction>;
 public:
     Components components;
+    ComponentFunctionVector functions;
     template <is_a_component T, is_an_emitter ... Emitters>
-    static constexpr std::vector<T> unpack_components(Emitters const & ...emitter_pack) { return { get<T>(emitter_pack)... }; }
-    explicit EmitterMemory(is_an_emitter auto &...emitter_pack)
-        : components { unpack_components<Ts>(emitter_pack...)... } { };
+    static constexpr std::vector<T> unpack_components(Emitters const & ...emitter_pack) { return { std::get<T>(emitter_pack.components)... }; }
+    static constexpr ComponentFunction unpack_function(is_an_emitter auto &emitter) { return emitter.component_function; }
+    template<is_an_emitter ... Emitters>
+    explicit EmitterMemory(Emitters const &...emitter_pack)
+        : components { unpack_components<Ts>(emitter_pack...)... },
+          functions { unpack_function(emitter_pack)... } { };
 };
 
 template <is_a_component ... Components, is_an_emitter ... Emitters>
--- main.cpp
+++ main.cpp
@@ -15,18 +15,20 @@ int main() {
     auto window = raylib::Window{config::kScreenWidth, config::kScreenHeight, "Basilevs"};
     auto audio = raylib::AudioDevice();
 
+    auto emitter_func = [](const double, World &, components::Position, components::ActiveComponent, components::ShootComponent){};
     auto emitter1 = TEmitter<components::Position, components::ActiveComponent, components::ShootComponent>{10};
-    auto &position1 = get<components::Position>(emitter1);
+    auto &&position1 = get<components::Position>(emitter1);
     position1.position = {10.0f, 10.0f};
     auto &active1 = get<components::ActiveComponent>(emitter1);
     active1.is_active = true;
-    auto emitter2 = TEmitter<components::Position, components::ActiveComponent, components::ShootComponent>{20};
+    auto emitter2 = TEmitter<components::Position, components::ActiveComponent, components::ShootComponent>(20, emitter_func);
     auto &position2 = get<components::Position>(emitter2);
     position2.position = {20.0f, 20.0f};
     auto &active2 = get<components::ActiveComponent>(emitter2);
     active2.is_active = false;
-    auto memory = EmitterMemory<components::Position, components::ActiveComponent, components::ShootComponent>(emitter1, emitter2);
-    auto position_components = get<components::Position>(memory);
+    auto memory = EmitterMemory(emitter1, emitter2);
+    auto &position_components = get<components::Position>(memory);
+    position_components[0].position = {50.0f, 50.0f};
     auto active_components = get<components::ActiveComponent>(memory);
 
   //  basilevs::Game::run(window, audio);
